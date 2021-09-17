4f572954f409352691fadce8ccf5859387652f19
daanzu <daanzu@gmail.com>
2021-09-17 05:21:13 -0400

    export_trained_model: fix bugs

--- export_trained_model.py
+++ export_trained_model.py
@@ -1,20 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse, os, shutil
 
 parser = argparse.ArgumentParser(description='Export trained model.')
 parser.add_argument('type', choices=('personal', 'finetune'), help='Type of trained model.')
-parser.add_argument('output_dir', default='exported_model', help='Directory to save the output model.')
+parser.add_argument('output_dir', nargs='?', default='exported_model', help='Directory to save the output model.')
 parser.add_argument('-b', '--base_model_dir', default='kaldi_model_daanzu_20200905_1ep-mediumlm-base', help='Directory of model to copy base files from.')
+parser.add_argument('-f', '--force', action='store_true', help='Force output: remove any existing output directory.')
 args = parser.parse_args()
 
 if not os.path.exists(args.base_model_dir):
     raise Exception('Base model directory does not exist.')
 if os.path.exists(args.output_dir):
-    raise Exception('Output directory already exists.')
-os.mkdir(args.output_dir, exist_ok=True)
+    if args.force:
+        shutil.rmtree(args.output_dir)
+    else:
+        raise Exception('Output directory already exists.')
 shutil.copytree(args.base_model_dir, args.output_dir, ignore=shutil.ignore_patterns('dict', 'tree_stuff'))
-os.mkdir(os.path.join(args.output_dir, 'training'), exist_ok=True)
+os.makedirs(os.path.join(args.output_dir, 'training'), exist_ok=True)
 
 if args.type == 'personal':
     for name in 'final.mdl tree'.split():
