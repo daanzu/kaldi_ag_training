ba5299d8ceedc87dc793afbe34e55c749122ed34
daanzu <daanzu@gmail.com>
2021-09-18 06:24:51 -0400

    add normalize_tsv script

---
+++ normalize_tsv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse, os, re
+
+parser = argparse.ArgumentParser(description="Normalize a TSV file's transcripts.")
+parser.add_argument('filename', help='Dataset TSV file to normalize.')
+args = parser.parse_args()
+
+if not os.path.exists(args.filename):
+    raise Exception('File does not exist: %s' % args.filename)
+
+def normalize_script(script):
+    script = re.sub(r'[\-]', ' ', script)
+    script = re.sub(r'[,.?!:;"]', '', script)
+    return script.strip().lower()
+
+lines = []
+with open(args.filename, 'r') as f:
+    for line in f:
+        fields = line.strip().split('\t')
+        text = fields[4]
+        text = normalize_script(text)
+        fields[4] = text
+        lines.append(fields)
+
+with open(args.filename, 'w') as f:
+    for line in lines:
+        f.write('\t'.join(line) + '\n')
