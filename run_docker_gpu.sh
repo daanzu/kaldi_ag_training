142d434706a1a9aac309da86490e93b99b00693d
daanzu <daanzu@gmail.com>
2021-11-13 04:49:21 -0500

    split out run_docker scripts for gpu & not

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -1,3 +1,3 @@
 docker run -it --rm -v $(pwd):/mnt/input -w /mnt/input --user "$(id -u):$(id -g)" \
-    daanzu/kaldi_ag_training:2020-11-28 \
+    --runtime=nvidia daanzu/kaldi_ag_training_gpu:2020-11-28 \
     $*
